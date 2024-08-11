d3c14e1a8480d613f741f29b09df227b57e7223d
Elandré van der Walt <elandre@rq-it.com>
2024-08-11 22:48:46 +0200

    Startup Credential Validation

--- credentials.py
+++ credentials.py
@@ -10,3 +10,11 @@ mqtt_broker = os.getenv("MQTT_BROKER")
 mqtt_port = os.getenv("MQTT_PORT")
 mqtt_topic = os.getenv("MQTT_TOPIC")
 
+
+for var_name, var_value in locals().copy().items():
+    if "mqtt_" in var_name or "tbb_" in var_name:
+        if var_value is None:
+            raise ValueError(f"{var_name} environment variable is not set.")
+
+
+
